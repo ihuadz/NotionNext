7cdd6e36cd7cc7f26fd453f927392bc0c3926fce
JessonGuitar <jessonyang1999@outlook.com>
2026-05-18 15:41:03 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -25,7 +25,7 @@ module.exports = {
   SPOILER_TEXT_TAG: process.env.NEXT_PUBLIC_SPOILER_TEXT_TAG || '', // Spoiler文本隐藏功能，如Notion中 [sp]希望被spoiler的文字[sp]，填入[sp] 即可
 
   // 音乐播放插件
-  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
   MUSIC_PLAYER_VISIBLE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
   MUSIC_PLAYER_AUTO_PLAY:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
@@ -36,20 +36,24 @@ module.exports = {
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
-    {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
-      cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    },
-    {
-      name: '王都グランセル',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
-      cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
-    }
+  {
+    name: '航行 Voyage',
+    artist: '杨家胜 | Jesson Yang',
+    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E8%88%AA%E8%A1%8C%20Voyage.mp3',
+    cover: ''
+  },
+  {
+    name: '处女座幻想 Virgo Fantasy',
+    artist: '杨家胜 | Jesson Yang',
+    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E5%A4%84%E5%A5%B3%E5%BA%A7%E5%B9%BB%E6%83%B3%20Virgo%20Fantasy.mp3',
+    cover: ''
+  },
+  {
+    name: '伴你左右 beside you',
+    artist: '杨家胜 | Jesson Yang',
+    url: 'https://cdn.jsdelivr.net/gh/JessonGuitar/mp3-cdn@main/%E4%BC%B4%E4%BD%A0%E5%B7%A6%E5%8F%B3%20beside%20you.mp3',
+    cover: ''
+  }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
